5f4c584fd76e2f9ec77276d4d45020d45128e291
JacksonSmith43 <jessica444@gmx.at>
2024-10-28 11:24:22 +0100

    Looks like a chessboard again and the pieces can be moved.

--- src/App.js
+++ src/App.js
@@ -10,50 +10,66 @@ export default function Game() {
 }
 
 function Board() {
-  const [squares, setSquares] = useState(Array(64).fill(null));
-  const initialPieces = {
-    0: "images/tower_black.png",
-    1: "images/knight_black.png",
-    2: "images/laufer_black.png",
-    3: "images/queen_black.png",
-    4: "images/king_black.png",
-    5: "images/laufer_black.png",
-    6: "images/knight_black.png",
-    7: "images/tower_black.png",
-    8: "images/pawn_black.png",
-    9: "images/pawn_black.png",
-    10: "images/pawn_black.png",
-    11: "images/pawn_black.png",
-    12: "images/pawn_black.png",
-    13: "images/pawn_black.png",
-    14: "images/pawn_black.png",
-    15: "images/pawn_black.png",
-    48: "images/pawn_white.png",
-    49: "images/pawn_white.png",
-    50: "images/pawn_white.png",
-    51: "images/pawn_white.png",
-    52: "images/pawn_white.png",
-    53: "images/pawn_white.png",
-    54: "images/pawn_white.png",
-    55: "images/pawn_white.png",
-    56: "images/tower_white.png",
-    57: "images/knight_white.png",
-    58: "images/laufer_white.png",
-    59: "images/queen_white.png",
-    60: "images/king_white.png",
-    61: "images/laufer_white.png",
-    62: "images/knight_white.png",
-    63: "images/tower_white.png",
+  const initialBoard = Array(64).fill(null);
+
+  initialBoard[0] = 'images/tower_black.png';
+  initialBoard[1] = 'images/knight_black.png';
+  initialBoard[2] = 'images/laufer_black.png';
+  initialBoard[3] = 'images/queen_black.png';
+  initialBoard[4] = 'images/king_black.png';
+  initialBoard[5] = 'images/laufer_black.png';
+  initialBoard[6] = 'images/knight_black.png';
+  initialBoard[7] = 'images/tower_black.png';
+
+  initialBoard[8] = 'images/pawn_black.png';
+  initialBoard[9] = 'images/pawn_black.png';
+  initialBoard[10] = 'images/pawn_black.png';
+  initialBoard[11] = 'images/pawn_black.png';
+  initialBoard[12] = 'images/pawn_black.png';
+  initialBoard[13] = 'images/pawn_black.png';
+  initialBoard[14] = 'images/pawn_black.png';
+  initialBoard[15] = 'images/pawn_black.png';
+
+  initialBoard[48] = 'images/pawn_white.png';
+  initialBoard[49] = 'images/pawn_white.png';
+  initialBoard[50] = 'images/pawn_white.png';
+  initialBoard[51] = 'images/pawn_white.png';
+  initialBoard[52] = 'images/pawn_white.png';
+  initialBoard[53] = 'images/pawn_white.png';
+  initialBoard[54] = 'images/pawn_white.png';
+  initialBoard[55] = 'images/pawn_white.png';
+
+  initialBoard[56] = 'images/tower_white.png';
+  initialBoard[57] = 'images/knight_white.png';
+  initialBoard[58] = 'images/laufer_white.png';
+  initialBoard[59] = 'images/queen_white.png';
+  initialBoard[60] = 'images/king_white.png';
+  initialBoard[61] = 'images/laufer_white.png';
+  initialBoard[62] = 'images/knight_white.png';
+  initialBoard[63] = 'images/tower_white.png';
+
+
+
+  const [squares, setSquares] = useState(initialBoard);
+  const [fromIndex, setFromIndex] = useState(null);
+
+  const handleDragStart = (index) => { // Saves the index of the current picked figure. 
+    setFromIndex(index);
   };
 
+  const handleDrop = (toIndex) => { // Moves the figure to the new field and empties the previous field. 
+    if ((fromIndex !== null) && (fromIndex !== toIndex)) {
+      const updatedSquares = [...squares];
+      updatedSquares[toIndex] = updatedSquares[fromIndex]; // Figur zum Ziel verschieben
+      updatedSquares[fromIndex] = null; // Empties the previous field. 
+      setSquares(updatedSquares); // Updates the condition. 
 
-  Object.keys(initialPieces).forEach((key) => (squares[key] = initialPieces[key]));
+      setFromIndex(null); // Resets the previous field. 
+    }
+  };
 
-  const movePiece = (fromIndex, toIndex) => {
-    const newSquares = [...squares];
-    newSquares[toIndex] = newSquares[fromIndex];
-    newSquares[fromIndex] = null;
-    setSquares(newSquares);
+  const handleDragOver = (event) => {
+    event.preventDefault(); // Allows drop events. 
   };
 
   return (
@@ -61,44 +77,32 @@ function Board() {
       {squares.map((image, index) => (
         <Square
           key={index}
-          index={index}
           image={image}
-          movePiece={movePiece}
+          index={index}
+          handleDragStart={() => handleDragStart(index)}
+          handleDrop={() => handleDrop(index)}
+          handleDragOver={handleDragOver}
         />
       ))}
     </div>
   );
 }
 
-function Square({ index, image, movePiece }) {
-  const handleDragStart = (e) => {
-    e.dataTransfer.setData("fromIndex", index);
-  };
-
-  const handleDrop = (e) => {
-    const fromIndex = e.dataTransfer.getData("fromIndex");
-    movePiece(fromIndex, index);
-  };
-
-  const handleDragOver = (e) => e.preventDefault();
-
+function Square({ image, handleDragStart, handleDrop, handleDragOver }) {
   return (
-    <div className="square"
+    <div
+      className="square"
       onDragOver={handleDragOver}
       onDrop={handleDrop}
-      style={{
-        // backgroundColor: (index % 2 === 0) ? 'white' : 'gray',
-      }}
     >
       {image && (
         <img
-          className="chess-piece"
           src={image}
-          alt="piece"
+          alt="chess piece"
           draggable
           onDragStart={handleDragStart}
         />
       )}
-    </div >
+    </div>
   );
 }
